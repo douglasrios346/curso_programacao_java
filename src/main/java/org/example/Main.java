3b20e0f0724dd0cbf63d89399c0bc4e5b8e9622c
douglasrios2007 <douglasrios2007@hotmail.com>
2022-12-11 13:46:11 -0300

    Exercicio01

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String product1 = "Computer";
+        String product2 = "Office desk";
+
+        int age = 30;
+        int code = 5290;
+        char gender = 'F';
+
+        double prince1 = 2100.0;
+        double prince2 = 650.50;
+        double measure = 53.234567;
+
+        System.out.println("Products:");
+        System.out.printf("%s, which price is $ %.2f%n", product1, prince1);
+        System.out.printf("%s, which price is $ %.2f%n", product2, prince2);
+
+        System.out.printf("%nRecord: %d years old, code %d and gender: %s", age, code , gender);
+
+        System.out.printf("%nMeasue with eight decimal places: %.8f", measure);
+        System.out.printf("%nRouded (three decimal places): %.3f", measure);
+        Locale.setDefault(Locale.US);
+        System.out.printf("%nUS decimal point: %.3f", measure);
+
+
+
+
+    }
+}
